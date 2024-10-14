896333d13ae533a0389538c41d85018a03ab4a34
Emi3312 <113268254+Emi3312@users.noreply.github.com>
2024-10-14 14:12:21 -0600

    AR_HEART

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2091467.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 199681068, 199681078, 199681071, 199681121, 199681069, 199681075, 199681125, 199681128, 199681122, 199681130, 199681088, 199681124, 199681115, 199681132 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/199681108/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/199681110/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/199681123/1/ammo.js', 'preload' : true},
+];
